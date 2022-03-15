fa285ea09c0a239fed884ae012777b0fb6fea9a7
Alex Betts <bettsalex1@gmail.com>
2022-03-15 09:58:04 +0000

    Change sphere color to normal values

--- src/main.rs
+++ src/main.rs
@@ -45,18 +45,24 @@ fn main() {
     //write_out_image(image_width, image_height, pixels);
 }
 
-fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
     let oc: Vec3 = r.origin - center;
     let a = Vec3::dot(r.direction, r.direction);
     let b = 2.0 * Vec3::dot(oc, r.direction);
     let c = Vec3::dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        Color::new(1.0, 0.0, 0.0)
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = Vec3::unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        0.5 * Color::new(n.r + 1.0, n.g + 1.0, n.b + 1.0)
     } else {
         let unit_direction: Vec3 = Vec3::unit_vector(r.direction);
         let t = 0.5 * (unit_direction.g + 1.0);
